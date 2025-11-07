5d5ec830b2f18602b05257fbeba3bedb8e454e9e
jaishankarg24 <jaishankarg24@gmail.com>
2025-11-07 07:56:10 +0530

    Spring IoC container

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,16 +2,16 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 
-		//SpringApplication.run(DemoApplication.class, args);
-//		var orderService = new OrderService(new StripePaymentService());
-		var orderService = new OrderService();
-		orderService.setPaymentService(new StripePaymentService());
+		ApplicationContext context = SpringApplication.run(DemoApplication.class, args);
+		OrderService orderService = context.getBean(OrderService.class);
 		orderService.placeOrder();
 	}
 
