b9bb373a2f17bdb642808eebaa73958da8a86a96
jaishankarg24 <jaishankarg24@gmail.com>
2025-11-07 08:27:03 +0530

    Controlling bean selection.

--- src/main/java/com/example/demo/OrderService.java
+++ src/main/java/com/example/demo/OrderService.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -8,14 +9,10 @@ public class OrderService {
 
     private PaymentService paymentService;
 
-    public OrderService(){};
-
-    @Autowired
-    public OrderService(PaymentService paymentService){
+    public OrderService(@Qualifier("paypal") PaymentService paymentService){
         this.paymentService = paymentService;
 
     }
-
 
     public void placeOrder() {
         paymentService.processPayment(10);
