2e30536a8e038487db44a594e6cfa21672cf3827
jaishankarg24 <jaishankarg24@gmail.com>
2025-11-09 06:10:48 +0530

    Simplifying code with Lombok

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import com.example.demo.entities.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -11,8 +12,21 @@ public class DemoApplication {
 	public static void main(String[] args) {
 
 
-		ConfigurableApplicationContext context = SpringApplication.run(DemoApplication.class, args);
+		//ApplicationContext context = SpringApplication.run(DemoApplication.class, args);
 
+		//3.
+		User user = User.builder()
+						.name("Jai")
+						.email("jai@gmail.com")
+						.password("pwd")
+						.build();
+		//2.
+		//User user = new User(1L, "jai", "jai@gmail", "pwd");
+
+		//1.
+		//user.setName("jai");
+		//user.setEmail("email");
+		//user.setPassword("pwd");
 	}
 
 }
